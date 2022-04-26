01f0dd92d0cd04ad327430a78cd257c72b49bad0
Mario Francesco Rovito <alessandro.r14@gmail.com>
2022-04-26 15:52:13 +0200

    add header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,23 @@
 <template>
-  <header>
-  
+  <header class="container">
+    <div class="row">
+      <div class="col-10 d-flex justify-content-start">
+        <li v-for="(element, i) in menuSx" :key="i" class="m-3">
+          <a href="#">{{ element }}</a>
+        </li>
+      </div>
+
+      <div class="col-2 d-flex justify-content-between align-items-center">
+        <span><i class="fa-brands fa-facebook"></i></span>
+        <span><i class="fa-brands fa-instagram"></i></span>
+        <span><i class="fa-brands fa-twitter"></i></span>
+        <span><i class="fa-brands fa-youtube"></i></span>
+      </div>
+    </div>
+
     <div class="d-flex justify-content-center p-3">
       <div class="p-2">
-        <img src= "../assets/img/avada-food-logo.png" alt="Logo" />
+        <img src="../assets/img/avada-food-logo.png" alt="Logo" />
       </div>
     </div>
     <div>
@@ -11,8 +25,7 @@
         <li v-for="(element, i) in menu" :key="i" class="m-3">
           <a href="#">{{ element }}</a>
         </li>
-         <span class="my-3"><i class="fa-solid fa-magnifying-glass"></i></span>
-      
+        <span class="my-3"><i class="fa-solid fa-magnifying-glass"></i></span>
       </ul>
     </div>
   </header>
@@ -25,6 +38,7 @@ export default {
   data() {
     return {
       menu: ["Home", "Recipes", "Places", "Blog", "About", "Contact"],
+      menuSx: ["Dowload App ", "Advertise with us"],
     };
   },
 };
