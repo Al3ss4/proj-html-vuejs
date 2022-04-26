3467e601e4253ea44576a387026db1612a1e25a9
Mario Francesco Rovito <alessandro.r14@gmail.com>
2022-04-26 16:02:24 +0200

    add main and jumbo comp

---
+++ src/components/JumboTron.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+export default {
+  nome: "jumboTron",
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <JumboTron />
+</template>
+
+<script>
+import JumboTron from './JumboTron.vue'
+export default {
+  components: { JumboTron },
+name:"mainComp"
+}
+</script>
+
+<style>
+
+</style>
