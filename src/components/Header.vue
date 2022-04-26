bb430cac80d64987577e4ed2e8e8339d12edce63
Mario Francesco Rovito <alessandro.r14@gmail.com>
2022-04-26 15:34:34 +0200

    add header menu and logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,37 @@
 <template>
-  <div></div>
+  <header>
+  
+    <div class="d-flex justify-content-center p-3">
+      <div class="p-2">
+        <img src= "../assets/img/avada-food-logo.png" alt="Logo" />
+      </div>
+    </div>
+    <div>
+      <ul class="d-flex text-uppercase fw-bold justify-content-center">
+        <li v-for="(element, i) in menu" :key="i" class="m-3">
+          <a href="#">{{ element }}</a>
+        </li>
+         <span class="my-3"><i class="fa-solid fa-magnifying-glass"></i></span>
+      
+      </ul>
+    </div>
+  </header>
 </template>
+
 
 <script>
 export default {
-name: 'headerComp'
-}
+  name: "HeaderComp",
+  data() {
+    return {
+      menu: ["Home", "Recipes", "Places", "Blog", "About", "Contact"],
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style lang="scss">
+li {
+  font-size: 14px;
+}
+</style> 
