f24cd8f5d7b8bef2036f30719533c301a8d49b45
Mario Francesco Rovito <alessandro.r14@gmail.com>
2022-04-28 12:42:16 +0200

    fix code

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,6 +5,7 @@
         <li v-for="(element, i) in menuSx" :key="i" class="m-3">
           <a href="#">{{ element }}</a>
         </li>
+        <span class="apple text-center"> iOS</span>
       </div>
 
       <div class="col-2 d-flex justify-content-between align-items-center social">
@@ -22,7 +23,7 @@
     </div>
     <div>
       <ul class="d-flex fw-bold justify-content-between col-12 mt-3">
-        <li class="m-3 grid" v-for="(element, i) in menu" :key="i" :class="{'grid': element.menu === 'Home'}">
+        <li class="m-3 grid" v-for="(element, i) in menu" :key="i">
           <a href="#">{{ element }}</a>
         </li>
         <span class="my-3"><i class="fa-solid fa-magnifying-glass"></i></span>
@@ -37,20 +38,12 @@ export default {
   name: "HeaderComp",
   data() {
     return {
+     
       menu: ["Home", "Recipes", "Places", "Blog", "About", "Contact"],
-      menuSx: ["Dowload App ", "Advertise with us"],
+      menuSx: ["Advertise with us", "Dowload App "],
     };
   },
-  methods:{
-    style(i){
-      switch(i) {
-        case 'Home':
-          return{
-            backgroundColor:'grid'
-          }
-      }
-    }
-  }
+ 
 };
 </script>
 
@@ -69,8 +62,16 @@ li {
   font-size: 14px;
 
 }
-.grid{
-  color:red
+.apple{
+  width: 30px;
+  background-color: #fc7525;
+  height: 20px;
+  color: white;
+  font-size: 0.7rem;
+  margin-top: 18px;
+  border-radius: 5px;
+  padding-top: 2px;
+  cursor: pointer;
 }
 
 
